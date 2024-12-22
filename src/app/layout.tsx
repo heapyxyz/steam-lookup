57852a5e68cf88ace4bdf75ba3609888741829bd
heapy <63428380+heapyxyz@users.noreply.github.com>
2024-12-23 00:13:37 +0100

    feat: add stuff to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,37 @@ export const viewport = {
 
 export const metadata: Metadata = {
   title: "SteamLookup",
-  description: "Tool for looking up Steam profiles.",
+  description: "Website for looking up Steam profiles",
+  generator: "Next.js",
+  applicationName: "SteamLookup",
+  authors: [{ name: "heapy", url: "https://heapy.xyz" }],
+  creator: "heapy",
+  publisher: "heapy",
+  keywords: [
+    "steam",
+    "steamlookup",
+    "steam lookup",
+    "steam api",
+    "steam profile",
+    "steam profiles",
+    "steam account",
+    "steam accounts",
+    "steam user",
+    "steam users",
+    "steam community",
+    "steam profile analysis",
+    "steam profile viewer",
+    "steam profile checker",
+    "steam profile lookup",
+    "steam id",
+    "steam id lookup",
+    "steam 64 id",
+    "steam 64 id lookup",
+    "steam profile stats",
+    "steam profile information",
+    "steam profile data",
+    "steam profile insights",
+  ],
   icons: {
     icon: [
       {
@@ -34,6 +64,16 @@ export const metadata: Metadata = {
         href: "/icon-dark.png",
       },
     ],
+  },
+  twitter: {
+    card: "app",
+    title: "SteamLookup",
+    description: "Website for looking up Steam profiles",
+    creator: "@heapyxyz",
+    images: {
+      url: "https://heapy.xyz/icon-dark.png",
+      alt: "SteamLookup Logo",
+    },
   },
 }
 
