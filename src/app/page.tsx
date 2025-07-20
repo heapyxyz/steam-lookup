2253890c8007f0f5a3ec8c46dfa2643eb664fcca
heapy <63428380+heapyxyz@users.noreply.github.com>
2025-07-20 19:19:20 +0200

    refactor(ui): improve home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import { GithubIcon } from "lucide-react"
-import Link from "next/link"
 import Image from "next/image"
 
 import Center from "@/components/center"
 import SearchForm from "@/components/search-form"
-import { buttonVariants } from "@/components/ui/button"
 import {
   Card,
   CardContent,
@@ -16,69 +13,67 @@ import {
 
 export default function Home() {
   return (
-    <Center>
-      <Card className="max-w-lg w-full">
-        <CardHeader>
-          <CardTitle className="text-2xl flex items-center gap-2">
-            <Image src="/icon-dark.png" alt="Logo" width={24} height={24} />
-            SteamLookup
-          </CardTitle>
+    <>
+      <div
+        className="fixed top-0 left-0 -z-50 w-full h-dvh bg-center bg-no-repeat bg-cover"
+        style={{
+          backgroundImage:
+            "url(https://steamcommunity-a.akamaihd.net/public/images/profile/2020/bg_dots.png)",
+        }}
+      />
 
-          <CardDescription>
-            The easiest way to lookup Steam profiles.
-          </CardDescription>
-        </CardHeader>
+      <Center>
+        <Card className="max-w-lg w-full bg-transparent border-0 backdrop-blur-2xl backdrop-brightness-50">
+          <CardHeader>
+            <CardTitle className="text-2xl flex items-center gap-2">
+              <Image src="/icon-dark.png" alt="Logo" width={24} height={24} />
+              SteamLookup
+            </CardTitle>
 
-        <CardContent className="text-muted-foreground text-sm">
-          <b>Supported Inputs</b>
+            <CardDescription>
+              The easiest way to lookup Steam profiles.
+            </CardDescription>
+          </CardHeader>
 
-          <ul>
-            <li>All SteamID variants, for example:</li>
+          <CardContent className="text-muted-foreground text-sm">
+            <b>Supported Inputs</b>
+
             <ul>
-              <li>76561198875567402</li>
-              <li>STEAM_1:0:457650837</li>
-              <div className="max-md:hidden">
-                <li>[U:1:915301674]</li>
-                <li>U:1:915301674</li>
-              </div>
+              <li>All SteamID variants:</li>
+              <ul>
+                <li>76561198875567402</li>
+                <li>STEAM_1:0:457650837</li>
+                <div className="max-md:hidden">
+                  <li>[U:1:915301674]</li>
+                  <li>U:1:915301674</li>
+                </div>
+              </ul>
+
+              <li>Custom IDs:</li>
+              <ul>
+                <li>heapyxyz</li>
+                <li>gaben</li>
+                <div className="max-md:hidden">
+                  <li>fjmv-jhdp</li>
+                </div>
+              </ul>
+
+              <li>Short and long URLs:</li>
+              <ul>
+                <div className="max-md:hidden">
+                  <li>https://.../profiles/76561198875567402</li>
+                  <li>https://.../id/heapyxyz</li>
+                </div>
+                <li>https://s.team/p/fjmv-jhdp</li>
+              </ul>
             </ul>
+          </CardContent>
 
-            <li>Custom IDs, for example:</li>
-            <ul>
-              <li>heapyxyz</li>
-              <li>gaben</li>
-              <div className="max-md:hidden">
-                <li>fjmv-jhdp</li>
-              </div>
-            </ul>
-
-            <li>Short and long URLs, for example:</li>
-            <ul>
-              <div className="max-md:hidden">
-                <li>https://.../profiles/76561198875567402</li>
-                <li>https://.../id/heapyxyz</li>
-              </div>
-              <li>https://s.team/p/fjmv-jhdp</li>
-            </ul>
-          </ul>
-        </CardContent>
-
-        <CardFooter className="flex-col gap-2">
-          <SearchForm />
-
-          <Link
-            href="https://github.com/heapyxyz/steam-lookup"
-            target="_blank"
-            className={buttonVariants({
-              variant: "outline",
-              className: "w-full",
-            })}
-          >
-            <GithubIcon />
-            GitHub
-          </Link>
-        </CardFooter>
-      </Card>
-    </Center>
+          <CardFooter className="flex-col gap-2">
+            <SearchForm />
+          </CardFooter>
+        </Card>
+      </Center>
+    </>
   )
 }
